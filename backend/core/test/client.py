1ff18351f764acf877ab76c4efb6aaf4cdb66ec5
Jackson90 <freshman118720@qq.com>
2020-04-29 11:29:55 +0800

    Delete something useless

--- backend/core/test/client.py
+++
@@ -1,4 +0,0 @@
-import socket
-
-c_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-c_sock.connect(('127.0.0.1', 8081))
--- backend/core/test/server.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-
-while True:
-    s_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s_sock.bind(('', 8081))
-    s_sock.listen(5)
